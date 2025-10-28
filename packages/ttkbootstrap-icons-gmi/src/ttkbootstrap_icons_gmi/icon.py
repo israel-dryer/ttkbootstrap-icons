c21cf4af44f9171fbdd360888656aceb1fc1d0fb
Israel Dryer <israel.dryer@gmail.com>
2025-10-28 01:45:31 -0400

    add typing for style parameters #21

--- packages/ttkbootstrap-icons-gmi/src/ttkbootstrap_icons_gmi/icon.py
+++ packages/ttkbootstrap-icons-gmi/src/ttkbootstrap_icons_gmi/icon.py
@@ -1,10 +1,14 @@
+from typing import Literal, Optional
+
 from ttkbootstrap_icons.icon import Icon
 from .provider import GoogleMaterialProvider
 
 
+GMIStyle = Literal["baseline", "outlined", "round", "sharp", "twotone"]
+
+
 class GMIIcon(Icon):
-    def __init__(self, name: str, size: int = 24, color: str = "black", style: str | None = None):
+    def __init__(self, name: str, size: int = 24, color: str = "black", style: Optional[GMIStyle] = None):
         GoogleMaterialProviderInstance = GoogleMaterialProvider()
         Icon.initialize_with_provider(GoogleMaterialProviderInstance, style=(style or "baseline").lower())
         super().__init__(name, size, color)
-
