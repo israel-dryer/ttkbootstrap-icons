6822aa7c20eabccbf1f9d65168c070a35b83e8ac
Israel Dryer <israel.dryer@gmail.com>
2025-10-28 23:43:51 -0400

    add visual test for lucide icons #32

---
+++ visual_tests/lucide.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
+import random
+
+from ttkbootstrap_icons_lucide import LucideIcon
+from ttkbootstrap_icons_lucide.provider import LucideFontProvider
+
+
+def main():
+    random.seed()
+    app = tk.Tk()
+    app.title("Lucide Icons")
+
+    prov = LucideFontProvider()
+    idx = prov.build_display_index()
+    names = idx.get("names", [])
+
+    frame = ttk.LabelFrame(app, text="default")
+    frame.pack(padx=10, pady=10, fill="x", expand=True)
+
+    if names:
+        a = LucideIcon(random.choice(names))
+        ttk.Label(frame, image=a.image, compound="left", text="default").pack(padx=10, pady=10, side="left")
+
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
